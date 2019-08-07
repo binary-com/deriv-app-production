6c2cb6d0f4506ae0defb68320f643719678b3343
negar <negar@binary.com>
2019-08-07 09:56:43 +0800

    Updates

---
+++ js/65.8d80b11c6ac6ffb87751.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[65],{574:function(a,e,l){"use strict";l.r(e);var s=l(1),n=l.n(s),t=l(0),c=l.n(t),i=function(a){var e=a.className;return c.a.createElement("svg",{className:e,width:"16",height:"16"},c.a.createElement("path",{className:"color1-fill",fillOpacity:".8",fillRule:"evenodd",d:"M7.5 11.793V3.5a.5.5 0 0 1 1 0v8.293l2.646-2.647a.5.5 0 1 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 11.793z"}))};i.propTypes={className:n.a.string},e.default=i}}]);
+//# sourceMappingURL=65.8d80b11c6ac6ffb87751.js.map
