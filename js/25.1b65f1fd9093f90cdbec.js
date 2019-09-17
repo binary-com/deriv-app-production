ed77ac208c1421110e8dab1afb355d1e24d6afe4
ashkan <ashkan.nemati@gmail.com>
2019-09-17 09:39:50 +0200

    Updates

--- js/25.1b65f1fd9093f90cdbec.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{541:function(e,a,n){"use strict";n.r(a);var l=n(5),i=n.n(l),s=n(1),t=n.n(s),c=n(0),o=n.n(c),r=function(e){var a=e.className;return o.a.createElement("svg",{className:i()("inline-icon",a),width:"16",height:"16",viewBox:"0 0 16 16"},o.a.createElement("path",{className:"color1-fill",fill:"#000",fillOpacity:"0.8",fillRule:"nonzero",d:"M0 7h16v2H0z"}))};r.propTypes={className:t.a.string},a.default=r}}]);
-//# sourceMappingURL=25.1b65f1fd9093f90cdbec.js.map
