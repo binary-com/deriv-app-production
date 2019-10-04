49e61107ac0779c5cdc7bf619eba153dc36f83f5
ashkan <ashkan.nemati@gmail.com>
2019-10-04 11:27:34 +0200

    Updates

--- js/81.bc7b064c6fa5082a9403.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[81],{1168:function(e,a,s){"use strict";s.r(a);var t=s(1),n=s.n(t),r=s(0),w=s.n(r),c=function(e){var a=e.className;return w.a.createElement("svg",{className:a,width:"50",height:"19",xmlns:"http://www.w3.org/2000/svg"},w.a.createElement("path",{d:"M31 11H0V8h31V0l19 9.5L31 19z",fill:"#FF444F"}))};c.propTypes={className:n.a.string},a.default=c}}]);
-//# sourceMappingURL=81.bc7b064c6fa5082a9403.js.map
