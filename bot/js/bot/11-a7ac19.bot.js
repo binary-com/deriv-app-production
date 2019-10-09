c2b051c78b46b1b8daa9cb619ae976bd72527ff0
negar <negar@binary.com>
2019-10-09 14:22:46 +0800

    Updates

---
+++ bot/js/bot/11-a7ac19.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[11],{583:function(e){e.exports=JSON.parse('{"text":["Here is where you can decide to sell your contract before it expires. Only one copy of this block is allowed.","Example:"]}')}}]);
+//# sourceMappingURL=11-a7ac19.bot.js.map
---
+++ bot/js/bot/27-35c2e4.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[27],{599:function(e){e.exports=JSON.parse('{"text":["This block returns specified candle value for given candle. Each candle has open price, close price, high price, low price, and open time.","This block requires a candle as an input parameter.","In the below example open price is extracted from the candle into the variable \\"op\\". The Candle data is provided by another block (Get candle)."]}')}}]);
+//# sourceMappingURL=27-35c2e4.bot.js.map
