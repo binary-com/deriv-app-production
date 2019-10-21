88b1fa3c3592c2c023a49a67f57aa886d8f8d8d0
negar <negar@binary.com>
2019-10-21 14:29:43 +0800

    Updates

---
+++ bot/js/bot/32-68931e.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[32],{609:function(e){e.exports=JSON.parse('{"text":["Prompts for an input of either text or numbers with a specific display message.","User input is being passed to selected variable and can be used later."]}')}}]);
+//# sourceMappingURL=32-68931e.bot.js.map
---
+++ bot/js/bot/33-b7fd1d.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[33],{610:function(e){e.exports=JSON.parse('{"text":["Any blocks placed within this block will be executed at every tick. If the default candle interval is set to 1 minute in the Trade Parameters root block, the instructions in this block will be executed once every minute. Place this block outside of any root block."]}')}}]);
+//# sourceMappingURL=33-b7fd1d.bot.js.map
