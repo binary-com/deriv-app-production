7321aa303fbe3377210ef563aef5a01384e39f7e
negar <negar@binary.com>
2019-11-08 15:23:42 +0800

    Updates

---
+++ bot/js/42.dcec3445e25490fc49b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{593:function(e,a,n){"use strict";n.r(a);var l=n(6),i=n.n(l),s=n(2),t=n.n(s),c=n(0),o=n.n(c),r=function(e){var a=e.className;return o.a.createElement("svg",{className:i()("inline-icon",a),width:"16",height:"16",viewBox:"0 0 16 16"},o.a.createElement("path",{className:"color1-fill",fill:"#000",fillOpacity:"0.8",fillRule:"nonzero",d:"M0 7h16v2H0z"}))};r.propTypes={className:t.a.string},a.default=r}}]);
+//# sourceMappingURL=42.dcec3445e25490fc49b3.js.map
